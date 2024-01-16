a12ab1449541c94fe0a8ec1dd35e7f936d12d600
ParadoksI <wwesuper146@gmail.com>
2024-01-16 15:23:33 +0900

    Добавление страницы

---
+++ js/category__slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.chead__slider').slick ({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        centerPadding: '30px',
+    });
+
+    $('.clist__item__group__img').slick ({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+    });
+});
